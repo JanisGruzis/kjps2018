a785af157efcc322eb9d935c70dccf2ff2903a1a
JanisGruzis <jgruzis@gmail.com>
2018-06-07 00:41:09 +0300

    index.php fixed

--- example/website/index.php
+++ example/website/index.php
@@ -1,16 +1,11 @@
-<?php
-
-$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
-
-switch ($request_uri[0])
-{
-    case '/':
-        require 'pages/home.php';
-        break;
-    case '/about':
-        require 'pages/about.php';
-        break;
-    default:
-        header('HTTP/1.0 404 Not Found');
-        break;
-}
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <a href="/pages/home.php">Home</a>
+        <br/>
+        <a href="/pages/about.php">About</a>
+    </body>
+</html>
